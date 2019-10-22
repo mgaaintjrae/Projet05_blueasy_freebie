cdf8c1100286c666ac0b5e6a56b5349e2c452d95
mgaaintjrae <pr.priscilla.roy@gmail.com>
2019-10-22 20:01:47 +0000

    [final] projet terminé

--- assets/js/script.js
+++ assets/js/script.js
@@ -176,84 +176,8 @@ formulaire.addEventListener("submit", function (e) {
 		return false;
 	}
 
-	/*
-		if (verifFullname() == true) {
-			console.log("Envoie le formulaire");
-		} if (verifMail() == true) {
-			console.log("Envoie le formulaire");
-		} if (verifMessage() == true) {
-			console.log("Envoie le formulaire");
-		} else {
-			alert("Le formulaire n'a pas pu être envoyé !");
-		}
-		*/
 		
 
 });
 
 
-
-/* ****************************   FORMULAIRE   ****************************** 
-******************************  AUTRE METHODE   *****************************
-window.onload=function(){
-	var formulaire = document.getElementById("formEnregistrer");
-	formulaire.addEventListener("submit", verifFullName);
-	}
-	function verifFullName(event) {
-	
-		event.preventDefault();
-	
-		var error = document.getElementById("errorFormulaire");
-		var fullname = document.getElementById("fullname");
-		var email = document.getElementById("email");
-		var message = document.getElementById("message");
-		var errs = [];
-		
-		if(fullname.value.length  == 0){
-			//alert("Le champ Full name est vide");
-	
-			errs.push("Le champ Full name est obligatoire");
-		}
-		else  if (fullname.value.length < 3 || fullname.value.length > 50){
-			//alert("problème largeur");
-			errs.push("Le champ Full name doit etre compris entre 3 et 50 caracteres");
-		}
-		
-		if(email.value.length  == 0){
-			//alert("Le champ email est vide");
-			errs.push("Le champ email est obligatoire");
-		}
-		else  if (email.value.length < 3 || email.value.length > 50){
-			//alert("problème largeur");
-			errs.push("Le champ email doit etre compris entre 3 et 50 caracteres");
-		}
-	
-		
-			if(message.value.length  == 0){
-			//alert("Le champ message est vide");
-			errs.push("Le champ message est obligatoire");
-		}
-		else  if (message.value.length < 3 || message.value.length > 250){
-			//alert("problème largeur");
-			errs.push("Le champ message doit etre compris entre 3 et 250 caracteres");
-		}
-	  
-		if(errs.length > 0){
-			console.log("erreur");
-			var html = "<div class='alert alert-danger' role='alert'>";
-			html = html + "<ul>";
-			errs.forEach(function(erreur, index, array) {
-				html = html + "<li>" +erreur + "</li>";
-			});
-			
-			html = html + "<ul>";
-			//alert(html);
-			error.innerHTML  = html;
-		
-		}
-		else{
-			error.innerHTML = "Ok";
-		
-		}  
-	}
-*/
